b915eb76edb17cead76ba71b059b8cdfc5671cf5
Teoh Yee Xian <yeexian_tyx@hotmail.com>
2023-06-03 22:26:05 +0800

    fix

--- lib/param_utils/load_params.py
+++ lib/param_utils/load_params.py
@@ -1,6 +1,7 @@
 from flax.serialization import msgpack_restore
 
 def load_params(filename):
+    print("filename")
     with open(filename, 'rb') as f:
         serialized_params = f.read()
     return msgpack_restore(serialized_params)
